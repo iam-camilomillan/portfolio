8d5721712aebb7f95b089ce16a9a6a21fa215929
CoGuisMod <coguismod@gmail.com>
2023-09-21 12:31:43 -0500

    Initial next project with pages folder.

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import ThemeButton from "./components/theme-button";
-
-export default function Home() {
-  return (
-    <main>
-      <ThemeButton />
-    </main>
-  );
-}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,30 +1,20 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
-  darkMode: "class",
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
   theme: {
-    screens: {
-      xxs: "512px",
-      xs: "576px",
-      sm: "672px",
-      md: "768px",
-      lg: "896px",
-    },
     extend: {
-      colors: {
-        "clr-primary-one": "#FFFFFF",
-        "clr-primary-two": "#000000",
-        "clr-secondary-one": "#FEFAE0",
-        "clr-secondary-two": "#B48155",
-        "clr-secondary-three": "#4A2F1C",
-      },
-      fontFamily: {
-        playfair_display: ["var(--font-playfair_display)"],
-        source_sans_pro: ["var(--font-source_sans_pro)"],
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
